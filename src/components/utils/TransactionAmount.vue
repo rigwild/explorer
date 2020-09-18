5e86d2c5872e215b62d29e290edd5dad649d6ba8
Pierre Seznec <pierre.seznec@spacelephant.org>
2020-09-18 16:42:57 +0200

    fix: show voucher rewards in foundation wallet

--- src/components/utils/TransactionAmount.vue
+++ src/components/utils/TransactionAmount.vue
@@ -18,7 +18,7 @@
       }"
       class="ml-auto"
     >
-      <span v-if="!isFee && isIncoming">+</span>
+      <span v-if="!isFee && isIncoming && source > 0">+</span>
       <span v-else-if="!isFee && isOutgoing && source > 0">-</span>
       {{ readableCrypto(source) }}
     </span>
@@ -42,6 +42,7 @@ import { CoreTransaction, MagistrateTransaction, TypeGroupTransaction } from "@/
 import { mapGetters } from "vuex";
 import { getMilestone } from "./utils";
 import { DIDType, DIDHelpers } from "@uns/ts-sdk";
+import { Identities } from "@uns/ark-crypto";
 
 @Component({
   computed: {
@@ -57,11 +58,19 @@ export default class TransactionAmount extends Vue {
   private initialBlockHeight = 0;
   private networkConfig;
   private initialMilestone = null;
+  private foundationWallet: string = null;
 
   @Watch("height")
   public onHeightChanged(newValue: number, oldValue: number) {
     if (!oldValue) {
       this.setInitialBlockHeight();
+    }
+  }
+
+  private setFoundationWallet() {
+    if (!this.foundationWallet) {
+      const foundationPubKey = this.networkConfig.network.foundation.publicKey;
+      this.foundationWallet = Identities.Address.fromPublicKey(foundationPubKey, this.networkConfig.network.pubKeyHash);
     }
   }
 
@@ -109,7 +118,15 @@ export default class TransactionAmount extends Vue {
       this.setInitialMilestone();
       const rewards = this.initialMilestone.voucherRewards[type.toLowerCase()];
 
-      if (this.transaction.recipient === this.$route.params.address) {
+      // Forge factory wallet
+      const issuerAddress = this.transaction.recipient;
+      if (this.$route.params.address === issuerAddress) {
+        return this.transaction.amount;
+      }
+
+      // Foundation wallet
+      this.setFoundationWallet();
+      if (this.$route.params.address === this.foundationWallet) {
         return rewards.foundation;
       }
       return rewards.sender + rewards.forger;
@@ -168,6 +185,18 @@ export default class TransactionAmount extends Vue {
         this.transaction.asset.payments.find((payment) => payment.recipientId === this.$route.params.address) &&
         (this.transactionTab === "received" || this.transaction.sender !== this.$route.params.address)
       );
+    }
+
+    // @ts-ignore
+    if (this.isUnsCertifiedNftMint(this.transaction.type, this.transaction.typeGroup)) {
+      // @ts-ignore
+      if (this.isVoucherUnsCertifiedNftMint(this.transaction)) {
+        return true;
+      }
+      // forge factory case
+      if (this.$route.params.address === this.transaction.recipient) {
+        return true;
+      }
     }
 
     // @ts-ignore
